5ff13fc8ff5fb2af333f4fb0e779e7cfe7fabceb
Burak <46630197+biednyinzynier@users.noreply.github.com>
2019-06-21 17:26:50 +0300

    Delete SecurityWebApplicationInitializer.java

--- spring-security-demo-01/src/main/com.burak.spring.security.demo/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.burak.springsecurity.demo.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
-	// special class to register the Spring security filters
-}
